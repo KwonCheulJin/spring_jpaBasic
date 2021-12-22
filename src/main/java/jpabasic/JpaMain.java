1d95e292ceef19030eb135282b55d636a3b32a5c
charles <chkftm12@gmail.com>
2021-12-22 21:15:32 +0900

    영속성 컨텍스트(Entity Manager), 변경 감지(Dirty checking)

--- src/main/java/jpabasic/JpaMain.java
+++ src/main/java/jpabasic/JpaMain.java
@@ -16,20 +16,45 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
         try {
-            Member findMember = em.find(Member.class, 1L);
+//            Member findMember = em.find(Member.class, 1L);
 //            System.out.println("findMember.getId() = " + findMember.getId());
 //            System.out.println("findMember.getName() = " + findMember.getName());
 //            em.remove(findMember);
 //            em.persist(member);
 //            findMember.setName("HelloJPA");
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                .getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                .getResultList();
+//            for (Member member1 : result) {
+//                System.out.println("member1 = " + member1.getName());
+//            }
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+            //비영속
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA2");
+            //영속
+//            System.out.println("=== BEFORE ===");
+//            em.persist(member);
+//            em.detach(member);
+//            System.out.println("=== AFTER ===");
+//            Member findMember1 = em.find(Member.class, 101L);
+//            Member findMember2 = em.find(Member.class, 101L);
 
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1.getName());
-            }
+//            System.out.println("result = " + (findMember1 == findMember2));
+
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//
+//            em.persist(member1);
+//            em.persist(member2);
+            //영속
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZZ");
+
+
+            System.out.println("=================");
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/jpabasic/Member.java
+++ src/main/java/jpabasic/Member.java
@@ -3,15 +3,21 @@ package jpabasic;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class Member {
 
     @Id
     private Long id;
     private String name;
 
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }

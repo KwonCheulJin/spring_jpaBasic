15efc4b3677f60ff36691f1be02e6f21e1b35942
charles <chkftm12@gmail.com>
2022-01-10 20:53:36 +0900

    데이터베이스 스키마 자동생성

--- src/main/java/jpabasic/Member.java
+++ src/main/java/jpabasic/Member.java
@@ -3,23 +3,21 @@ package jpabasic;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "MBR")
 @Getter
 @Setter
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class Member {
 
     @Id
     private Long id;
     private String name;
+    private int age;
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 }
